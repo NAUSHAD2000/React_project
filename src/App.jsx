21a8425266f82ccaf3192aa2c36ace244c2494dd
Md Naushad Alam <mdnalam.@gmail.com>
2025-10-16 20:12:07 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Calculator from './components/Calculator'
+
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Calculator/>
+    
+    </>
+    
+  )
+}
+
+export default App
